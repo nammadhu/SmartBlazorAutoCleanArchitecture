e446bc7b7d2370d67c995c9316b420cb4a62ee45
Madhu <Madhusudhan.V@outlook.in>
2025-03-06 10:03:42 +0530

    removed extra project of of smart -fluent ui

--- Source/Src/Presentation/Smart.Web/Data/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Smart.Web.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-{
-}
